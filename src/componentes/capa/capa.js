82386f0df4ed2b2015ccc499271c56a2bdaceba9
Jardel <tecjardelv@gmail.com>
2023-04-10 10:15:38 -0300

    ajustes de mostragem

--- src/componentes/capa/capa.js
+++ src/componentes/capa/capa.js
@@ -6,8 +6,8 @@ export default function Capa() {
         <div className="container-capa">
             <div className="container-content-capa">
                 <div className="container-sombra-item">
-                    <h1 className="container-item-capa">Systech Engenharia</h1>
-                    <p className="container-item-capa">Desenvolvendo soluções tecnológias para automação industrial</p>
+                    <h1 className="container-item-capa">SYSTECH ENGENHARIA</h1>
+                    <p className="container-item-capa">Desenvolvendo soluções tecnológicas para automação industrial</p>
                 </div>
             </div>
         </div>
