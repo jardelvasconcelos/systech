041f50b8808461e2001052ea2f237d48c048e258
Jardel <tecjardelv@gmail.com>
2023-04-11 10:52:45 -0300

    importacao de fonts e responsividade dos links do menu

--- src/componentes/capa/capa.js
+++ src/componentes/capa/capa.js
@@ -7,7 +7,7 @@ export default function Capa() {
             <div className="container-content-capa">
                 <div className="container-sombra-item">
                     <h1 className="container-item-capa">SYSTECH ENGENHARIA</h1>
-                    <p className="container-item-capa">Desenvolvendo soluções tecnológicas para automação industrial</p>
+                    <p className="container-item-content-capa">Desenvolvendo soluções tecnológicas para automação industrial</p>
                 </div>
             </div>
         </div>
