45a51e1fd14f6cb118deff70121e15bda555a662
Jardel <tecjardelv@gmail.com>
2023-04-12 22:55:56 -0300

    refatoracao e adicao de menu burguer

--- src/componentes/topo/topo.js
+++ src/componentes/topo/topo.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./topo.css"
 
 export default function Topo() {
+
+
+    const [openMenu, setOpenMenu] = useState(false);
+    function aparecer() {
+        setOpenMenu(!openMenu)
+    }
+
     return (
         <header className="container-topo">
             <div className="container-logo">
@@ -9,6 +16,15 @@ export default function Topo() {
                     <img className="img-logo" src="assets/systech-op-3.png" alt="logo" />
                 </a>
             </div>
+
+            <div className="container-item-buttom ">
+                <button onClick={aparecer} >
+                    <div className="menu_burger"></div>
+                    <div className="menu_burger"></div>
+                    <div className="menu_burger"></div>
+                </button>
+            </div>
+
             <nav className="container-link-topo">
                 <a href="" className="link-topo"><strong>SOBRE</strong> </a>
                 <a href="" className="link-topo"> <strong>SOLUÇÕES</strong> </a>
