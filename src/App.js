6a0c9c3c1a1232ce61b51d17a1b2b440ac8d4b53
Jardel <tecjardelv@gmail.com>
2023-04-13 16:46:30 -0300

    inicio de insercao do footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Topo from './componentes/topo/topo';
 import Capa from './componentes/capa/capa';
 import Conteudo from './componentes/conteudo/conteudo';
-
+import Footer from "./componentes/footer/footer";
 function App() {
   return (
     <div className="App">
       <Topo/>
       <Capa/>
       <Conteudo/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/componentes/footer/footer.js
@@ -0,0 +1,16 @@
+import "./footer.css";
+
+export default function Footer() {
+    return(
+        <footer className="container-footer">
+                <h2 className="coment-footer">Contatos</h2>
+                <div className="container-img-footer">
+                    <img className="img-item-footer" src="assets/fundo_preto.jpg" alt="" />
+                    <img className="img-item-footer"src="" alt="" />
+                    <img className="img-item-footer" src="" alt="" />
+                    <img className="img-item-footer" src="" alt="" />
+                </div>
+                <p>Jardel Vasconcelos</p>
+        </footer>
+    )    
+}
