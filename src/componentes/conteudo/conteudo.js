be02f3f619618e09f7bb936e643d79990fe8ad52
Jardel <tecjardelv@gmail.com>
2023-04-13 16:25:46 -0300

    correcao e incremento de imagens e estilos

--- src/componentes/conteudo/conteudo.js
+++ src/componentes/conteudo/conteudo.js
@@ -6,7 +6,7 @@ export default function Conteudo() {
         <main>
             <section className="container-sobre">
                 <div className="container-item-sobre">
-                    <h1 className="item-sobre-title">Sobre a Systech</h1>
+                    <h1 className="item-sobre-title"><span className="span-item-sobre-title">Sobre a Systech</span></h1>
                     <h2 className="item-sobre-subtitle">
                         Mollit velit ut cillum laboris culpa consequat
                         deserunt et dolore nulla reprehenderit dolor.
@@ -19,11 +19,21 @@ export default function Conteudo() {
                 </div>
                 <div className="container-img-sobre">
                     <img className="img-item-sobre" src="assets/kuka01.jpg" alt="kuka01" />
-                    {/*  <img className="img-item-sobre" src="" alt="" /> */}
                 </div>
             </section>
             <section className="container-solucoes">
-
+                <div className="container-item-sobre">
+                    <h1 className="item-sobre-title"><span className="span-item-sobre-title">Nossas Soluções</span></h1>
+                    <h2 className="item-sobre-subtitle">
+                        Mollit velit ut cillum laboris culpa consequat
+                        deserunt et dolore nulla reprehenderit dolor.
+                        Non et cillum magna cupidatat .</h2>
+                    <p className="item-sobre-text">Cillum deserunt tempor voluptate fugiat Lorem minim aliquip.
+                        Tempor adipisicing ea veniam in officia voluptate elit pariatur aute reprehenderit sunt sit exercitation ad.
+                        Magna duis ea et fugiat do ad elit id ea dolore officia et.
+                        Ad laboris dolor esse commodo.
+                        Adipisicing proident enim culpa voluptate esse nisi culpa.</p>
+                </div>
             </section>
         </main>
     )
