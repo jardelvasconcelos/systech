ba0293a4c362812885f145921c34b53d20634d2a
Jardel <tecjardelv@gmail.com>
2023-04-13 11:04:12 -0300

    edicao de imagem

--- src/componentes/conteudo/conteudo.js
+++ src/componentes/conteudo/conteudo.js
@@ -7,7 +7,10 @@ export default function Conteudo() {
             <section className="container-sobre">
                 <div className="container-item-sobre">
                     <h1 className="item-sobre-title">Sobre a Systech</h1>
-                    <h2 className="item-sobre-subtitle">Mollit velit ut cillum laboris culpa consequat deserunt et dolore nulla reprehenderit dolor. Non et cillum magna cupidatat .</h2>
+                    <h2 className="item-sobre-subtitle">
+                        Mollit velit ut cillum laboris culpa consequat
+                        deserunt et dolore nulla reprehenderit dolor.
+                        Non et cillum magna cupidatat .</h2>
                     <p className="item-sobre-text">Cillum deserunt tempor voluptate fugiat Lorem minim aliquip.
                         Tempor adipisicing ea veniam in officia voluptate elit pariatur aute reprehenderit sunt sit exercitation ad.
                         Magna duis ea et fugiat do ad elit id ea dolore officia et.
