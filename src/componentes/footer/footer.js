70502f9187f84d1a0be7bd8c439135aa6ebd982a
Jardel <tecjardelv@gmail.com>
2023-04-14 15:29:59 -0300

    insercao de blur e imagens de footer

--- src/componentes/footer/footer.js
+++ src/componentes/footer/footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
         <footer className="container-footer">
                 <h2 className="coment-footer">Contatos</h2>
                 <div className="container-img-footer">
-                    <img className="img-item-footer" src="assets/fundo_preto.jpg" alt="" />
-                    <img className="img-item-footer"src="" alt="" />
-                    <img className="img-item-footer" src="" alt="" />
-                    <img className="img-item-footer" src="" alt="" />
+                    <img className="img-item-footer" src="assets/fundo_preto.jpg" alt="a" />
+                    <img className="img-item-footer"src="assets/whatsapp.png" alt="b" />
+                    <img className="img-item-footer" src="assets/logotipo-do-gmail.png" alt="" />
+                    
                 </div>
                 <p>Jardel Vasconcelos</p>
         </footer>
