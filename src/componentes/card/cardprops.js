b424a5e41ccde84beac83fea1e141c5131f5da93
Jardel <tecjardelv@gmail.com>
2023-04-19 15:52:39 -0300

    continuacao de producao de cards

--- src/componentes/card/cardprops.js
+++ src/componentes/card/cardprops.js
@@ -1,13 +1,17 @@
+import "./card.css";
 
-export default function Card (props){
-    return(
-        <div className="container-card"> 
-            <div className="container-img-card">
-                <img className="img-card" src={props.icon} alt="image" />
+export default function Card(props) {
+    return (
+       
+            <div className="container-item-card">
+                <div className="container-img-card">
+                    <img className="img-card" src={props.icon} alt="image" />
+                    <p className="coment-card">{props.coment}</p>
+                </div>
+                {/* <div className="container-coment-card">
+                   
+                </div> */}
             </div>
-            <div className="container-coment-card">
-                <span className="coment-card">{props.coment}</span>
-            </div>
-        </div>
+        
     )
 }
--- src/componentes/card/soluction.js
+++ src/componentes/card/soluction.js
@@ -1,12 +1,22 @@
 import Card from './cardprops.js';
 
-export default function Soluction () {
-    return(
-    <Card
-    icon = ""
-    coment = "testando"
-    />
+export default function Soluction() {
+    return (
+        <section className='container-card'>
+            <Card
+                icon="assets/painel01.jpg"
+                coment="dsjkgsdçlkgçlsdkfjgçlfsdkgçldfsjçdflklkhlkhlkljçd"
+            />
+            <Card
+                icon="assets/painel01.jpg"
+                coment="dsjkgsdçlkgçlsdkfjgçlfsdkgçldfsjçdfljçdfslkhçlkh "
+            />
+            <Card
+                icon="assets/painel01.jpg"
+                coment="dsjkgsdçlkgçlsdkfjgçlfsdkgçldfsjçdfljçdfslkhçlkhf"
+            />
+        </section>
     )
-    
+
 }
 
