06c816930dfd823ba5ab96fb49cd77ffeeee8a6d
Jardel <tecjardelv@gmail.com>
2023-04-23 02:14:04 -0300

    inicio ajuste de menu

--- src/componentes/topo/topo.js
+++ src/componentes/topo/topo.js
@@ -10,29 +10,31 @@ export default function Topo() {
     }
 
     return (
-        <header className="container-topo">
-            <div className="container-logo">
-                <a href="">
-                    <img className="img-logo" src="assets/systech-op-3.png" alt="logo" />
-                </a>
-            </div>
-            <div className="container-item-buttom ">
-                <button onClick={aparecer} >
-                    <div className="menu_burger"></div>
-                    <div className="menu_burger"></div>
-                    <div className="menu_burger"></div>
-                </button>
-            </div>
-            <div className={openMenu ? 'dentro' : 'fora'}>
+        <section className="testes">
+            <header className="container-topo">
+                <div className="container-logo">
+                    <a href="">
+                        <img className="img-logo" src="assets/systech-op-3.png" alt="logo" />
+                    </a>
+                </div>
+                <div className="container-item-buttom ">
+                    <button onClick={aparecer} >
+                        <div className="menu_burger"></div>
+                        <div className="menu_burger"></div>
+                        <div className="menu_burger"></div>
+                    </button>
+
+                </div>
+                               <nav className="container-link-topo">
+                    <a href="" className="link-topo"><strong>SOBRE</strong> </a>
+                    <a href="" className="link-topo"> <strong>SOLUÇÕES</strong> </a>
+                    <a href="" className="link-topo"><strong>CONTATOS</strong> </a>
+                </nav>
+            </header>
+             <div className={openMenu ? 'dentro' : 'fora'}>
                 <div className="teste"></div>
             </div>
-            <nav className="container-link-topo">
-                <a href="" className="link-topo"><strong>SOBRE</strong> </a>
-                <a href="" className="link-topo"> <strong>SOLUÇÕES</strong> </a>
-                <a href="" className="link-topo"><strong>CONTATOS</strong> </a>
-            </nav>
-            
-        </header>
+        </section>
 
 
     )
